4eb3d4a641ac842b8d6ed8e144afe387a9a0dc24
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-04 07:16:25 -0700

    Add navigation links to Header component

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import { about } from "../../pages/about";
+import { home } from "../../pages/home";
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4">
       <h1 className="text-2xl font-bold">ALX Project</h1>
+      <Link href="/about">About</Link>
+      <Link href="/home">Home</Link>
     </header>
   );
 }
