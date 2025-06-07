c3b6ba17dfd88b726fd464c8cb8a8773532415fc
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-07 06:38:40 -0700

    Add link to Posts section in Header component for improved navigation

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
       <h1 className="text-2xl font-bold">ALX Project</h1>
       <Link href="/about">About</Link>
       <Link href="/home">Home</Link>
+      <Link href="/posts">Post</Link>
     </header>
   );
 }
