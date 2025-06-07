02cb2fd6a72da2eaff042a0ce71f2c64ba012e66
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-07 06:41:37 -0700

    Refactor Posts page to include Header component and enhance layout with a title and description for better user experience.

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Header from '../components/layout/Header';
 
-function posts() {
+const PostsPage = () => {
   return (
-    <div>posts</div>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow container mx-auto p-4">
+        <h1 className="text-3xl font-bold my-8">Latest Posts</h1>
+        <p>Browse our collection of posts.</p>
+      </main>
+    </div>
+  );
+};
 
-export default posts
+export default PostsPage;
