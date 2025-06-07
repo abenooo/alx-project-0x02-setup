7a12067d51b8e3359d6ee4f721692fccc0f9ec44
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-07 07:09:50 -0700

    Enhance PostData interface and introduce PostProps interface for better post management

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -14,10 +14,12 @@ export interface PostData {
   content: string;
 }
 export interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
   size?: 'small' | 'medium' | 'large';
   shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
-  className?: string;
-  type?: 'button' | 'submit' | 'reset';
+}
+export interface PostProps {
+  id: number,
+  title: string,
+  body: string;
+  userId: string;
 }
